87652da7f954f2efb59de7f39055affc0a724073
Justin Park <justinpark@Justins-MacBook-Pro.local>
2020-06-12 18:56:26 -0700

    adding initial file

---
+++ playground.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+# Playground for using RabbitMQ with Python (using Pika and Celery)
+
+
+import pika
+from celery import Celery
+
+
+# playground
+def main():
+    print(pika)
+
+
+if __name__ == "__main__":
+    main()
